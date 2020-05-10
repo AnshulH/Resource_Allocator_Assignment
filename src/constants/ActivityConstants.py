68de30545baee5b7221527c7780995e27cb98f2d
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-11 01:21:53 +0530

    Structuring files

---
+++ src/constants/ActivityConstants.py
@@ -0,0 +1,22 @@
+REGION = "region"
+
+TOTAL_COST = "total_cost"
+
+MACHINES = "machines"
+
+OUTPUT = "Output"
+
+MACHINES_DICT = {
+    10 : "Large",
+    20 : "XLarge",
+    40 : "2XLarge",
+    80 : "4XLarge",
+    160 : "8XLarge",
+    320 : "10XLarge",
+}
+
+REGION_LIST = [
+    "NY",
+    "IND",
+    "CHN"
+]
