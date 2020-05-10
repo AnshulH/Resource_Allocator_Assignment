62ddc50d0fb2c6342cec0dff45847fe46c2e1a60
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-10 21:59:57 +0530

    Added method to track items in optimal set

--- knapsack.py
+++ knapsack.py
@@ -12,6 +12,31 @@ def knapsack(W, wt, val, n):
 
     return dp[n][W], dp
 
+"""
+    Recursively reconstructs one of the optimal subsets given
+    a filled DP table and the vector of weights
+    Parameters
+    ---------
+    dp: list of list, the table of a solved integer weight dynamic programming problem
+    wt: list or tuple, the vector of weights of the items
+    i: int, the index of the  item under consideration
+    j: int, the current possible maximum weight
+    optimal_set: set, the optimal subset so far. This gets modified by the function.
+    Returns
+    -------
+    None
+"""
+def _construct_solution(dp: list, wt: list, i: int, j: int, optimal_set: dict):
+    if i > 0 and j > 0:
+        if dp[i - 1][j] == dp[i][j]:
+            _construct_solution(dp, wt, i - 1, j, optimal_set)
+        else:
+            if(wt[i-1] in optimal_set):
+                optimal_set[wt[i-1]] += 1
+            else:
+                optimal_set[wt[i-1]] = 1
+            _construct_solution(dp, wt, i, j - wt[i - 1], optimal_set)
+
 
 if __name__ == "__main__":
     """
@@ -24,6 +49,6 @@ if __name__ == "__main__":
     F = [[0] * (w + 1)] + [[0] + [-1 for i in range(w + 1)] for j in range(n + 1)]
     optimal_solution, dp = knapsack(w, wt, val, n)
     Set = dict()
-    # _construct_solution(dp,wt,n,w,Set)
+    _construct_solution(dp,wt,n,w,Set)
     print(Set)
     print(optimal_solution)
