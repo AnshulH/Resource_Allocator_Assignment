8495ec9f885c1c865a70ef22cf194d7fa4d3e44b
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-11 09:50:52 +0530

    Update test file

--- src/test/test_OutputJson.py
+++ src/test/test_OutputJson.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../')
 
 from main import mainFunc
-
+# TODO: Fix issue with args to run complete test
 # class TestKnapsackMethod(unittest.TestCase): 
 #     def test_weight_value(self):
 #         self.assertEquals(mainFunc(['main.py', '--units', '1000', '--hours', '1']),1)
