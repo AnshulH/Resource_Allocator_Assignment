283473999204fef86581c84909be1a371f6f2973
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-11 13:00:34 +0530

    Update OutputFormatHelper.py

--- src/utilities/OutputFormatHelper.py
+++ src/utilities/OutputFormatHelper.py
@@ -5,6 +5,8 @@ sys.path.append('../')
 
 import constants.ActivityConstants as activityConstants
 
+# Method which generates the required format of json from the region map 
+# region map -> {'NY': {'cost': 8850, 'subset': {160: 6, 40: 1}}, 'IND': {'cost': 8213, 'subset': {160: 6, 40: 1}}, 'CHN': {'cost': 7480, 'subset': {160: 6, 20: 2}}}
 def OutputJson(regionMap):
     outputJson = dict()
     outputMap = list()
@@ -17,7 +19,8 @@ def OutputJson(regionMap):
     outputJson["Output"] = outputMap
     jsonString = json.dumps(outputJson, indent=4)
     return jsonString
-
+# Maps coresponding subset map to machine based on units 
+# ex. LARGE -> 10 units
 def getMachineMap(subset):
     machineMap = dict()
     for machine in subset.keys():
