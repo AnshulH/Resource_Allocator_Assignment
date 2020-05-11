1450e513980a456a19f4f350a1b660a6cab70e61
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-11 15:00:41 +0530

    Update test_ResourceAllocation.py

--- src/test/utilities/test_ResourceAllocation.py
+++ src/test/utilities/test_ResourceAllocation.py
@@ -1,7 +1,7 @@
 import sys
 import unittest
 sys.path.append('../../')
-from utilities.resource_allocation import knapsack
+from utilities.ResourceAllocation import knapsack
 
 
 class TestKnapsackMethod(unittest.TestCase):
@@ -13,9 +13,6 @@ class TestKnapsackMethod(unittest.TestCase):
         self.assertRaises(AssertionError, knapsack, 11, [1], [1, 2])
 
     def test_weight_negative_value(self):
-        self.assertRaises(AssertionError, knapsack, -10, [1], [1])
-
-    def test_n_value(self):
         self.assertRaises(AssertionError, knapsack, -10, [1], [1])
 
 
