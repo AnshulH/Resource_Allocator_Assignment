e9eaec9e062e1a1023a7006ccdccbda8d3206c57
Anshul Hajare <anshulhajare09@gmail.com>
2020-05-11 14:28:51 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import json
-from utilities.input_parser import cli_input
-from utilities.resource_allocation import knapsack, construct_solution
-from utilities.output_format_helper import output_json
+from utilities.ArgParser import cli_input
+from utilities.ResourceAllocation import knapsack, construct_solution
+from utilities.OutputFormatHelper import output_json
 from constants import activity_constants as ACTIVITY_CONSTANTS
 
 inventory_file = open('constants/regionwise_inventory.json', )
 REGION_WISE_INVENTORY = json.load(inventory_file)
 
 
-def resourceAllocationEngine():
+def resource_allocation_engine():
     # Obtain inputs from commandline
     args = cli_input()
     units = args["units"]
@@ -43,4 +43,4 @@ def resourceAllocationEngine():
 
 
 if __name__ == "__main__":
-    resourceAllocationEngine()
+    resource_allocation_engine()
